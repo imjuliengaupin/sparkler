38e4054dea74eb97c76847b06f33a133e167cc22
Julien Gaupin <imjuliengaupin@gmail.com>
2025-03-21 18:24:48 -0400

    Implement Producer, Blueprint, and ProducerBlueprint classes

--- src/main/java/com/sandbox/kafka/Blueprint.java
+++ src/main/java/com/sandbox/kafka/Blueprint.java
@@ -1,6 +1,36 @@
 
 package com.sandbox.kafka;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Blueprint {
 
+    public Blueprint() {
+        // empty default constructor
+    }
+
+    public Map<String, String> getBlueprintMapping() throws Exception {
+        // use reflection to map class fields to their associated values being assigned
+        // in the class constructor
+        Map<String, String> blueprintMapping = new HashMap<String, String>();
+
+        for (Field classField : this.getClass().getDeclaredFields()) {
+            // reflected object should supress java language access checking when it is used
+            classField.setAccessible(true);
+
+            String key = classField.getName();
+            String value = classField.get(this).toString();
+
+            // append the field name and its value to the map
+            blueprintMapping.put(key, value);
+        }
+
+        if (blueprintMapping.isEmpty()) {
+            throw new Exception("properties not found in /src/main/resources");
+        }
+
+        return blueprintMapping;
+    }
 }
--- src/main/java/com/sandbox/kafka/ProducerBlueprint.java
+++ src/main/java/com/sandbox/kafka/ProducerBlueprint.java
@@ -1,6 +1,17 @@
 
 package com.sandbox.kafka;
 
-public class ProducerBlueprint {
+import java.util.Properties;
 
+public class ProducerBlueprint extends Blueprint {
+
+    public String keySerializer = null;
+    public String valueSerializer = null;
+    public String bootstrapServers = null;
+
+    public ProducerBlueprint(Properties properties) {
+        this.keySerializer = properties.getProperty("key.serializer");
+        this.valueSerializer = properties.getProperty("value.serializer");
+        this.bootstrapServers = properties.getProperty("bootstrap.servers");
+    }
 }
